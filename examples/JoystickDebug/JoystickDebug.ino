cb115aca3d4a413aec452b3da71ba438ffab41aa
Michael Aldridge <aldridge.mac@gmail.com>
2023-09-19 20:55:36 -0500

    examples/JoystickDebug: Add joystick debugger program.

---
+++ examples/JoystickDebug/JoystickDebug.ino
@@ -0,0 +1,44 @@
+#include "Gizmo.h"
+
+// The Gizmo provides access to the data that is held by the field
+// management system and the gizmo system processor.
+Gizmo gizmo;
+
+// Initialize the hardware and configure libraries for use.
+void setup() {
+  // Initialize the connection to the system processor.
+  gizmo.begin();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// Loop runs continuously.
+void loop() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  gizmo.refresh();
+
+  Serial.printf("Axis Data: %i %i %i %i %i %i %i %i; ",
+                gizmo.getAxis(GIZMO_AXIS_LX),
+                gizmo.getAxis(GIZMO_AXIS_LY),
+                gizmo.getAxis(GIZMO_AXIS_RX),
+                gizmo.getAxis(GIZMO_AXIS_RY),
+                gizmo.getAxis(GIZMO_AXIS_LT),
+                gizmo.getAxis(GIZMO_AXIS_RT),
+                gizmo.getAxis(GIZMO_AXIS_DX),
+                gizmo.getAxis(GIZMO_AXIS_DY)
+                );
+Serial.printf("Button Data: %i %i %i %i %i %i %i %i %i %i",
+              gizmo.getButton(GIZMO_BUTTON_BACK),
+              gizmo.getButton(GIZMO_BUTTON_START),
+              gizmo.getButton(GIZMO_BUTTON_LEFTSTICK),
+              gizmo.getButton(GIZMO_BUTTON_RIGHTSTICK),
+              gizmo.getButton(GIZMO_BUTTON_X),
+              gizmo.getButton(GIZMO_BUTTON_Y),
+              gizmo.getButton(GIZMO_BUTTON_A),
+              gizmo.getButton(GIZMO_BUTTON_B),
+              gizmo.getButton(GIZMO_BUTTON_LSHOULDER),
+              gizmo.getButton(GIZMO_BUTTON_RSHOULDER)
+              );
+  Serial.println();
+  delay(100);
+}
